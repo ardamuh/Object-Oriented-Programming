9573da28a4c2c500175dd53bfd8d48d851f03d10
Arda Muhammadan Arifin <59552433+ardamuh@users.noreply.github.com>
2021-06-22 21:12:45 +0700

    Final Project for lab OOP

---
+++ 8 puzzle Final Project/src/EightPuzzle/Node.java
@@ -0,0 +1,40 @@
+
+package EightPuzzle;
+
+import java.util.ArrayList;
+
+public class Node {
+    
+    private String state;
+    private ArrayList<Node> child;
+    private Node parent;
+
+    public Node(String state) {
+        this.state = state;
+        child = new ArrayList<Node>();
+    }
+    
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public ArrayList<Node> getChild() {
+        return child;
+    }
+
+    public void addChild(Node child1) {
+        child.add(child1);
+    }
+}
